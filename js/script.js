f8459661a6c602d9e5df7d79362fa05b19f07287
MirkoValle <mirko.valle20@gmail.com>
2024-05-21 17:23:33 +0200

    Added function to control if the user number are correct

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
 const randomNumber = randomNumberGenerator()
-console.log(randomNumber)
+const mainOut = document.querySelector("h1.main")
+let timer = 5;
+
+mainOut.innerHTML = "I numeri da ricordare sono: " + randomNumber
 
 
+setTimeout(function () {
+
+    mainOut.innerHTML = ""
+
+    controllo(randomNumber)
+
+
+
+
+}, 2000);
+
+
+
+
+
+
+
+function controllo(randomNumber) {
+    let correctNumbers = [];
+
+    for (let index = 0; index < 5; index++) {
+        let userNum = Number.parseInt(prompt("Inserisci il numero che ricordi"), 10);
+        if (randomNumber.includes(userNum) && (correctNumbers.includes(userNum) == false)) {
+            correctNumbers.push(userNum);
+        }
+
+    }
+    console.log(correctNumbers)
+}
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * ((max + 1) - min)) + min;
